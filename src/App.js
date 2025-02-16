925cd6edf457fa1c57b9a262faa985844d2fefa2
TaLeVinhCo <talevinhco1009@gmail.com>
2025-02-15 23:56:09 -0500

    Repair suceeded

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Student ID: 101507972</h1>
+      <h1>Student ID: Ta Le Vinh Co</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developerd By: Ta Le Vinh Co
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
